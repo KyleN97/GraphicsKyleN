a73ae5d583429589b5ddf6c13b478bb5a3294635
KyleN97 <kyle.norton1@live.com>
2017-03-20 15:26:48 +1100

    new fbx shader

--- Week_2/Landscape/bin/Shaders/fbxShader.fs
+++ Week_2/Landscape/bin/Shaders/fbxShader.fs
@@ -4,23 +4,56 @@ in vec2 vuv;
 in vec3 fPos; 
 out vec4 FragColor; 
 uniform sampler2D diffuseTexture;
-uniform float lightAmbientStrength;
-uniform vec3 lightPosition; 
-uniform vec3 lightColor; 
-uniform vec3 lightSpecColor; 
-uniform float specPower = 32.0f;
+uniform float[3] lightAmbientStrength;						
+uniform vec4 [3] lightPosition;								
+uniform vec3 [3] lightColor;	
+uniform vec3 [3] lightSpecColor;	
+uniform float[3] attenuation;
+uniform float[3] coneangle;
+uniform vec3 [3] coneDirection;	
+uniform float[3] specPower;
+uniform int[3] lightType;
 uniform vec3 camPos; 
-void main() { 
+void main()
+{
+	vec3 finalLighting = vec3(0,0,0);
+	vec3 lightDir;
+		for(int i = 0; i < 3; i++){
+		float attenuationFactor = 1.0;
+		if(lightType[i] == 0){
+			lightDir = normalize(lightPosition[i].xyz);
+			attenuationFactor = 1.0;
+			//Directional Light
+		}
+		else if(lightType[i] == 1){
+			//Point Light
+			lightDir = normalize(lightPosition[i].xyz - fPos);
+			attenuationFactor = 1.0;
+		}
+		else if (lightType[i] == 2){
+			lightDir = normalize(lightPosition[i].xyz - fPos);
+			float distanceToLight = length(lightPosition[i].xyz - fPos);
+			lightDir = normalize(lightPosition[i].xyz - fPos);				 
+			attenuationFactor = 1.0 / (1.0 + attenuation[i] * pow(distanceToLight,2));
+    		float lightToSurfaceAngle = degrees(acos(dot(-lightDir,normalize(coneDirection[i]))));
+			if(lightToSurfaceAngle > coneangle[i]){
+			  	attenuationFactor = 0.0;
+			}
+			//Spot Light
+		}
 
-		vec3 norm = normalize(vNormal.xyz);
-		vec3 lightDir = normalize(lightPosition - fPos);
-		float diff = max(dot(norm, lightDir), 0.0f);
-		vec3 diffColor = diff * lightColor; 
-		vec3 ambient = lightColor * lightAmbientStrength; 
-		vec3 R = reflect(-lightDir, norm);
-		vec3 E = normalize(camPos - fPos); 
-		float specTerm = pow(max(0.0f, dot(R, E)), specPower); 
-		vec3 Specular = lightColor * specTerm; 
-		FragColor = texture2D(diffuseTexture, vuv) * vec4(Specular + ambient + diffColor,1); 
-
-};
+		
+		vec3 norm 	= normalize(vNormal.xyz);		
+		float diff 	= max(dot(norm, lightDir), 0.0);
+		vec3 R		= reflect(-lightDir, norm);
+		vec3 E	    = normalize(camPos - fPos); 
+		float specTerm = pow(max(0.0, dot(R,E)), specPower[i]); 
+		
+		vec3 totalDiffuse  =  diff * lightColor[i]; 
+		vec3 finalAmbient  =  lightColor[i] * lightAmbientStrength[i]; 
+		vec3 totalSpecular =  lightSpecColor[i] * specTerm; 
+		finalLighting +=  finalAmbient * attenuationFactor * (totalDiffuse + totalSpecular);
+				
+	}
+	FragColor = texture2D(diffuseTexture, vuv) * vec4(finalLighting,1); 
+}
